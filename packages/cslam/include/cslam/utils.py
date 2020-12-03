b08350058bbd38543340266b8837763b960aeb35
Andrea F. Daniele <afdaniele@ttic.edu>
2020-12-03 11:38:00 -0600

    removed old code

--- packages/cslam/include/cslam/utils.py
+++ packages/cslam/include/cslam/utils.py
@@ -18,61 +18,6 @@ class TFMeasurement:
     tf: TF
 
 
-# @dataclasses.dataclass
-# class DuckietownMap:
-#     tile_size: float
-#     tiles: dict
-#     objects: dict
-#
-#     @property
-#     def width(self):
-#         return self.tile_size * len(self.tiles[0])
-#
-#     @property
-#     def height(self):
-#         return self.tile_size * len(self.tiles)
-#
-#     def tags(self, origin: str = 'world') -> List[TFMeasurement]:
-#         measurements = []
-#         # iterate over the floor tags
-#         for _, obj in self.objects.items():
-#             if obj['kind'] == "floor_tag":
-#                 tag_id = obj['tag']['~TagInstance']['tag_id']
-#                 position = obj['pose']['~SE2Transform']['p']
-#                 theta = 0
-#                 if "theta_deg" in obj['pose']['~SE2Transform']:
-#                     theta = obj['pose']['~SE2Transform']['theta_deg']
-#                 target = "tag/%d" % tag_id
-#                 # get position
-#                 t = np.array([position[0], position[1], 0.0])
-#                 # get rotation
-#                 Rz = geometry.rotation_from_axis_angle(np.array([0, 0, 1]), np.deg2rad(theta))
-#                 Rx = geometry.rotation_from_axis_angle(np.array([1, 0, 0]), np.deg2rad(180))
-#                 R = np.matmul(Rz, Rx)
-#                 q = geometry.quaternion_from_rotation(R)
-#                 # compile pose
-#                 measurements.append(TFMeasurement(
-#                     origin=origin,
-#                     target=target,
-#                     tf=TF(
-#                         t=t,
-#                         q=q
-#                     )
-#                 ))
-#         # ---
-#         return measurements
-#
-#
-# def load_map(map_filepath: str) -> DuckietownMap:
-#     with open(map_filepath, 'r') as fin:
-#         map_data = yaml.safe_load(fin)
-#         return DuckietownMap(
-#             tile_size=map_data['tile_size'],
-#             tiles=map_data['tiles'],
-#             objects=map_data['objects']
-#         )
-
-
 def create_info_matrix(stdd_position, stdd_orientation, constraints=None):
     if constraints is None:
         constraints = [True] * 6
