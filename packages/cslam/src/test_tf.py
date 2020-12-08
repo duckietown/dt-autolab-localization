5b8fa2b262fa7e317d3bc69739116d60866601b1
Andrea F. Daniele <afdaniele@ttic.edu>
2020-12-08 16:54:34 -0600

    disabled ROS in test-tf

--- packages/cslam/src/test_tf.py
+++ packages/cslam/src/test_tf.py
@@ -8,8 +8,8 @@ from dt_communication_utils import DTCommunicationGroup
 from autolab_msgs.msg import \
     AutolabTransform
 
-rospy.init_node('autolab_tf_listener')
-br = tf2_ros.TransformBroadcaster()
+# rospy.init_node('autolab_tf_listener')
+# br = tf2_ros.TransformBroadcaster()
 
 group = DTCommunicationGroup("/autolab/tf", AutolabTransform)
 
@@ -17,20 +17,20 @@ group = DTCommunicationGroup("/autolab/tf", AutolabTransform)
 def cb(msg, _):
     print(msg.origin.name, msg.target.name)
 
-    t = TransformStamped()
-
-    t.header.stamp = rospy.Time.now()
-    t.header.frame_id = msg.origin.name
-    t.child_frame_id = msg.target.name
-    t.transform = msg.transform
-
-    br.sendTransform(t)
+    # t = TransformStamped()
+    #
+    # t.header.stamp = rospy.Time.now()
+    # t.header.frame_id = msg.origin.name
+    # t.child_frame_id = msg.target.name
+    # t.transform = msg.transform
+    #
+    # br.sendTransform(t)
 
 
 print('Listening...')
 group.Subscriber(cb)
 
-rospy.on_shutdown(group.shutdown)
-
-rospy.spin()
+# rospy.on_shutdown(group.shutdown)
+#
+# rospy.spin()
 
