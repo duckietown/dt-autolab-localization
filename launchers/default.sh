20cd00a0d40576e8cdaa12cbef60546de33d73b2
Andrea F. Daniele <afdaniele@ttic.edu>
2021-05-14 16:13:08 -0500

    added --wait to roslaunch

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec roslaunch autolab-localization default.launch
+dt-exec roslaunch --wait autolab-localization default.launch
 
 
 # ----------------------------------------------------------------------------
