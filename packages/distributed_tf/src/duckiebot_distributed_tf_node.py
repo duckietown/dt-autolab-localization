e5e9f07e2e175fbab2fb40406a24202372f08c35
Andrea F. Daniele <afdaniele@ttic.edu>
2021-06-02 17:48:49 -0500

    improved distributed_tf, cleaned up code

--- packages/distributed_tf/src/duckiebot_distributed_tf_node.py
+++ packages/distributed_tf/src/duckiebot_distributed_tf_node.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import re
+import time
+
 import rospy
 import tf2_ros
 import threading
@@ -25,7 +27,6 @@ import tf.transformations as tr
 
 class DistributedTFNode(DTROS):
 
-    FETCH_TF_STATIC_EVERY_SECS = 60
     PUBLISH_TF_STATIC_EVERY_SECS = 2
 
     def __init__(self):
@@ -39,39 +40,13 @@ class DistributedTFNode(DTROS):
         self.tag_id = rospy.get_param('~tag_id')
         self.min_distance_odom = rospy.get_param('~min_distance_odom')
         self.max_time_between_poses = rospy.get_param('~max_time_between_poses')
-        self.transforms = rospy.get_param('~transforms', [])
-        # apply values to templated TFs
-        for tf in self.transforms:
-            tf['origin'] = tf['origin'].format(veh=self.robot_hostname, tag_id=self.tag_id)
-            tf['target'] = tf['target'].format(veh=self.robot_hostname, tag_id=self.tag_id)
         # define local reference frames' names
-        self._tag_frame = f'tag/{self.tag_id}'
+        self._tag_mount = f'{self.robot_hostname}/tag_mount'
         self._footprint_frame = f'{self.robot_hostname}/footprint'
         # create communication group
         self._group = DTCommunicationGroup("/autolab/tf", AutolabTransform)
         # create static tfs holder and access semaphore
-        self._static_tfs = {
-            (tf['origin'], tf['target']): AutolabTransform(
-                origin=AutolabReferenceFrame(
-                    time=rospy.Time(),
-                    type=AutolabReferenceFrame.TYPE_DUCKIEBOT_FOOTPRINT,
-                    name=tf['origin'],
-                    robot=self.robot_hostname
-                ),
-                target=AutolabReferenceFrame(
-                    time=rospy.Time(),
-                    type=AutolabReferenceFrame.TYPE_DUCKIEBOT_TAG,
-                    name=tf['target'],
-                    robot=self.robot_hostname
-                ),
-                is_fixed=True,
-                is_static=False,
-                transform=Transform(
-                    translation=Vector3(*tf['translation']),
-                    rotation=Quaternion(*tr.quaternion_from_euler(*tf['rotation']))
-                )
-            ) for tf in self.transforms
-        }
+        self._static_tfs = {}
         self._static_tfs_sem = threading.Semaphore(1)
         self._tf_buffer = tf2_ros.Buffer()
         self._tf_listener = tf2_ros.TransformListener(self._tf_buffer)
@@ -79,17 +54,11 @@ class DistributedTFNode(DTROS):
         self._tf_pub = self._group.Publisher()
         # fetch/publish right away and then set timers
         if self.tag_id != '__NOTSET__':
-            # self._fetch_static_tfs()
-            self._publish_static_tfs()
-            # TODO: disabled because TFs were added to the YAML config file since ROS does not like multiple URDFs combination
-            # self._tf_static_timer = rospy.Timer(
-            #     rospy.Duration(self.FETCH_TF_STATIC_EVERY_SECS),
-            #     self._fetch_static_tfs
-            # )
-            self._pub_timer = rospy.Timer(
-                rospy.Duration(self.PUBLISH_TF_STATIC_EVERY_SECS),
-                self._publish_static_tfs
-            )
+            threading.Thread(target=self._fetch_tag_tf).start()
+        self._pub_timer = rospy.Timer(
+            rospy.Duration(self.PUBLISH_TF_STATIC_EVERY_SECS),
+            self._publish_static_tfs
+        )
         # setup subscribers for odometry
         self._odo_sub = rospy.Subscriber(
             "~odometry_in",
@@ -104,46 +73,45 @@ class DistributedTFNode(DTROS):
         if hasattr(self, '_group') and self._group is not None:
             self._group.shutdown()
 
-    def _fetch_static_tfs(self, *_):
-        origin = self._tag_frame
+    def _fetch_tag_tf(self, *_):
+        origin = self._tag_mount
         target = self._footprint_frame
         # try to fetch the TF
-        try:
-            transform = self._tf_buffer.lookup_transform(origin, target, rospy.Time())
-            tf = AutolabTransform(
-                origin=AutolabReferenceFrame(
-                    time=transform.header.stamp,
-                    type=AutolabReferenceFrame.TYPE_DUCKIEBOT_TAG,
-                    name=origin,
-                    robot=self.robot_hostname
-                ),
-                target=AutolabReferenceFrame(
-                    time=transform.header.stamp,
-                    type=AutolabReferenceFrame.TYPE_DUCKIEBOT_FOOTPRINT,
-                    name=target,
-                    robot=self.robot_hostname
-                ),
-                is_fixed=True,
-                is_static=False,
-                transform=transform.transform
-            )
-            # add TF to the list of TFs to publish
-            self._static_tfs_sem.acquire()
+        while not self.is_shutdown:
             try:
-                self._static_tfs[(origin, target)] = tf
-            finally:
-                self._static_tfs_sem.release()
-            # stop timer
-            self.loginfo("All static TFs needed have been fetched. Disabling TF listener.")
-            # noinspection PyBroadException
-            try:
-                self._tf_static_timer.shutdown()
-            except BaseException:
+                self.loginfo(f"Looking for TF[{origin}] -> [{target}]...")
+                transform = self._tf_buffer.lookup_transform(origin, target, rospy.Time())
+                tf = AutolabTransform(
+                    origin=AutolabReferenceFrame(
+                        time=transform.header.stamp,
+                        type=AutolabReferenceFrame.TYPE_DUCKIEBOT_TAG,
+                        # NOTE: fetch TF /tag_frame -> /footprint but publish tag/XYZ -> /footprint
+                        name=f'tag/{self.tag_id}',
+                        robot=self.robot_hostname
+                    ),
+                    target=AutolabReferenceFrame(
+                        time=transform.header.stamp,
+                        type=AutolabReferenceFrame.TYPE_DUCKIEBOT_FOOTPRINT,
+                        name=target,
+                        robot=self.robot_hostname
+                    ),
+                    is_fixed=True,
+                    is_static=False,
+                    transform=transform.transform
+                )
+                # add TF to the list of TFs to publish
+                self._static_tfs_sem.acquire()
+                try:
+                    self._static_tfs[(origin, target)] = tf
+                finally:
+                    self._static_tfs_sem.release()
+                self.loginfo(f"Found TF[{origin}] -> [{target}]. Stopping TF listener thread.")
+                return
+            except (tf2_ros.LookupException, tf2_ros.ExtrapolationException):
+                self.logwarn(f"Could not find a static TF [{origin}] -> [{target}]")
+            except tf2_ros.ConnectivityException:
                 pass
-        except (tf2_ros.LookupException, tf2_ros.ExtrapolationException):
-            self.logwarn(f"Could not find a static TF [{origin}] -> [{target}]")
-        except tf2_ros.ConnectivityException:
-            pass
+            time.sleep(2)
 
     def _publish_static_tfs(self, *_):
         tfs = []
