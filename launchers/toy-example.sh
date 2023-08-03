4fc117196eed82ede7d48580968192caa0fecd29
Davide Iafrate <Tuxliri@users.noreply.github.com>
2023-08-03 17:21:43 -0400

    Moved toy example to assets folder

--- launchers/toy-example.sh
+++ launchers/toy-example.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec python3 -m cslam.tests.toy_example
+dt-exec python3 /code/toy-example.py
 
 
 # ----------------------------------------------------------------------------
